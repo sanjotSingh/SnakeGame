51ce11de7c0c93a6b1f147c25cb7fa43e0580bb9
Chandi <schandi@csus.edu>
2024-03-26 18:03:25 -0700

    First Commit

---
+++ app/src/main/java/com/example/snake/Button.java
@@ -0,0 +1,49 @@
+package com.example.snake;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.MotionEvent;
+
+import java.util.Random;
+
+public class Button {
+    // The location of the apple on the grid
+    // Not in pixels
+    private Point location = new Point();
+
+    // An image to represent the button
+    private Bitmap buttonBitmap;
+
+    // Objects for the game loop/thread
+    private Thread mThread = null;
+
+    // Is the game currently playing and or paused?
+    private volatile boolean mPlaying = false;
+
+
+    int x = 200;
+    int y = 40;
+
+    /// Set up the apple in the constructor
+    Button(Context context, Point sr, int s, int width, int height){
+        // Load the image to the bitmap
+        buttonBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.pause);
+        // Resize the bitmap
+        buttonBitmap = Bitmap.createScaledBitmap(buttonBitmap, width, height, false);
+
+
+    }
+
+    // Draw the apple
+    public void draw(Canvas canvas, Paint paint){
+        canvas.drawBitmap(buttonBitmap, x, y, paint);
+
+    }
+
+}
